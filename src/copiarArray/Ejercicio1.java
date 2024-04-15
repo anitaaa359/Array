51a181ed0e32cff28583ea5ddba204fab113bc49
anitaaa359 <166609237+anitaaa359@users.noreply.github.com>
2024-04-15 11:35:18 +0200

    Update Ejercicio1.java

--- src/copiarArray/Ejercicio1.java
+++ src/copiarArray/Ejercicio1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-
+//comentario de gitHub
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner (System.in);
